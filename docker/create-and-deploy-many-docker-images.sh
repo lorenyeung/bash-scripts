9f4a2a04b7b6b6eaabdaa2281dbcccf1f55f276d
Loren Yeung <lorenyeung@users.noreply.github.com>
2019-05-16 17:57:54 -0700

    allows for selecting number of custom layers

--- docker/create-and-deploy-many-docker-images.sh
+++ docker/create-and-deploy-many-docker-images.sh
@@ -9,14 +9,15 @@ org=0
 group=0
 
 # set these accordingly if you want to skip the select statements below
-artifactory=10.128.0.51:8081
-method=RepositoryPath
-repo=docker-local
-image=larger-test
+artifactory=docker.art
+method=Subdomain
+repo=docker
+image=load-test
 user=admin
 pass=password
-
-declare -a docker_id=("httpd" "docker" "python" "jetty" "node" "vault" "influxdb" "neo4j" "ghost" "sentry" "sonarqube" "jenkins" "maven" "logstash" "tomcat" "gradle" "jruby" "solr" "groovy" "elasticsearch" "openjdk" "nextcloud" "telegraf" "php" "traefik" "redmine" "wordpress" "owncloud" "drupal" "perl" "golang" "ruby" "rethinkdb" "redis" "rabbitmq" "postgres" "percona" "mongo" "memcached" "kibana" "mariadb" "haproxy" "cassandra" "buildpack-deps" "debian" "kong" "fedora" "ubuntu" "composer" "nats" "busybox" "amazonlinux" "consul" "registry" "alpine" "swarm" "java")
+rm $image-*
+declare -a docker_id=("scratch")
+#declare -a docker_id=("httpd" "docker" "python" "jetty" "node" "vault" "influxdb" "neo4j" "ghost" "sentry" "sonarqube" "jenkins" "maven" "logstash" "tomcat" "gradle" "jruby" "solr" "groovy" "elasticsearch" "openjdk" "nextcloud" "telegraf" "php" "traefik" "redmine" "wordpress" "owncloud" "drupal" "perl" "golang" "ruby" "rethinkdb" "redis" "rabbitmq" "postgres" "percona" "mongo" "memcached" "kibana" "mariadb" "haproxy" "cassandra" "buildpack-deps" "debian" "kong" "fedora" "ubuntu" "composer" "nats" "busybox" "amazonlinux" "consul" "registry" "alpine" "swarm" "java")
 
 # enter artifactory if not hard coded
 if [ -z "$artifactory" ]; then
@@ -30,35 +31,47 @@ fi
 
 # enter repo if not hard coded
 if [ -z "$repo" ]; then
-read -p "Please enter your Docker repository on Artifactory [docker-local]: " repo
-repo=${repo:-docker-local}
+	read -p "Please enter your Docker repository on Artifactory [docker-local]: " repo
+	repo=${repo:-docker-local}
 fi
 
 # choose method if not hard coded
 if [ -z "$method" ]; then
-    echo "Choose your docker method:" 
-    select yn in "RepositoryPath" "Subdomain" "Ports"; do
-        case $yn in
-            RepositoryPath ) method=$yn; break;;
-            Subdomain ) method=$yn; artifactory=$repo.$artifactory; break;;
-		        Ports ) method=$yn; break;;
-        esac
-    done
+	echo "Choose your docker method:" 
+	select yn in "RepositoryPath" "Subdomain" "Ports"; do
+		case $yn in
+			RepositoryPath ) method=$yn; break;;
+			Subdomain ) method=$yn; artifactory=$repo.$artifactory; break;;
+			Ports ) method=$yn; break;;
+		esac
+	done
 fi
 echo "You have selected $method"
-
 docker login $artifactory -u $user -p $pass
 declare -a size0=("5" "6" "7")
 declare -a size1=("1" "2" "3" "4" "5" "6" "7" "8" "9")
+declare -a layerCount=("1" "2" "3" "4" "5" "6" "7" "8" "9")
 
 for n in {1..90000}; do
    size1_sel=${size1[$RANDOM % ${#size1[@]}]}
    size0_sel=${size0[$RANDOM % ${#size0[@]}]}
-   head -c "$size0_sel$size1_sel"000000 </dev/urandom > $n-$image
+   layerCount_sel=${layerCount[$RANDOM % ${#layerCount[@]}]}
+   
+   m=1
+   while [ "$m" -le "$layerCount_sel" ]; do
+      head -c "$size0_sel$size1_sel"0 </dev/urandom > $image-$n-$m
+      let 'm++'
+      size1_sel=${size1[$RANDOM % ${#size1[@]}]}
+      size0_sel=${size0[$RANDOM % ${#size0[@]}]}
+   done
    selected=${docker_id[$RANDOM % ${#docker_id[@]} ]}
    touch Dockerfile
    echo "FROM $selected" > Dockerfile
-   echo "ADD $n-$image /" >> Dockerfile
+   m=1
+   while [ "$m" -le "$layerCount_sel" ]; do
+      echo "ADD $image-$n-$m /" >> Dockerfile
+      let 'm++'
+   done
    echo "Created $selected $size0_sel$size_sel size file"
    version=$(($version+1))
    epoch=$(date +%s)
@@ -68,7 +81,6 @@ for n in {1..90000}; do
 	Ports ) image_name=$artifactory/org-$org/group-$group/version-$version/$n-$image:$epoch; ;;
     esac   
     echo "Building image: $image_name"
-	exit
    if [ $version -ge 50 ]; then
        group=$(($group+1))
        version=0
@@ -81,6 +93,10 @@ for n in {1..90000}; do
    docker build . -t $image_name
    rm Dockerfile
    docker push $image_name
-   rm $n-$image
+   m=1
+   while [ "$m" -le "$layerCount_sel" ]; do
+   rm $image-$n-$m
+   let 'm++'
+   done
    docker rmi $image_name
 done
