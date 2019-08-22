923eac8e3d78e6a04020769d9764d72d52be0e86
Loren Yeung <lorenyeung@users.noreply.github.com>
2019-08-22 00:23:49 -0700

    Update reupload.sh

--- create-and-upload-many-files/reupload.sh
+++ create-and-upload-many-files/reupload.sh
@@ -2,5 +2,5 @@
 
 for f in run*; do
 	echo "$f"
-	./jfrog rt u "$f/" customer-$f-local/files/ --threads 10 --build-name name-$f --build-number number-$f  
+	./jfrog rt u "$f/" customer-$f-local/files/ --threads 10 --build-name name-$f --build-number number-$f --props "key1=$DATE;key2=$(openssl rand -hex 8);key3=$(openssl rand -hex 8);key4=$(openssl rand -hex 8)"
 done
